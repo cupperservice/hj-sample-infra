8bd285caa941da4c28c329686fadac4d6df0e41b
kawasima kazuhisa <cupper@mac.com>
2022-09-01 16:37:24 +0900

    change name

--- common/cloud_watch_log.tf
+++ common/cloud_watch_log.tf
@@ -1,10 +1,10 @@
-resource "aws_cloudwatch_log_group" "hj-sample-app/access" {
+resource "aws_cloudwatch_log_group" "hj-sample-app_access" {
   name = "hj-sample-app/access.log"
 
   retention_in_days = 1
 }
 
-resource "aws_cloudwatch_log_group" "hj-sample-app/application" {
+resource "aws_cloudwatch_log_group" "hj-sample-app_application" {
   name = "hj-sample-app/application.log"
 
   retention_in_days = 1
