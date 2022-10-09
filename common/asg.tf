2feeec6821db3b66dd8030267e3a10f002938797
kawasima kazuhisa <cupper@mac.com>
2022-10-09 10:36:32 +0900

    modify

--- common/asg.tf
+++ common/asg.tf
@@ -35,6 +35,6 @@ resource "aws_autoscaling_group" "hj-sample" {
 }
 
 resource "aws_autoscaling_attachment" "asg_attachment_bar" {
-  autoscaling_group_name = aws_autoscaling_group.hj-sample.id
-  lb_target_group_arn    = aws_lb_target_group.alb.arn
+  autoscaling_group_name  = aws_autoscaling_group.hj-sample.id
+  alb_target_group_arn    = aws_lb_target_group.alb.arn
 }
