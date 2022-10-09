0025a31292bd4c6a87431891148a0ea5ec093791
kawasima kazuhisa <cupper@mac.com>
2022-10-09 09:33:15 +0900

    change target type

--- common/alb.tf
+++ common/alb.tf
@@ -11,7 +11,7 @@ resource "aws_alb_target_group" "alb" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = "${aws_vpc.main.id}"
-  target_type = "ip"
+  target_type = "instance"
 
   health_check {
     interval            = 30
