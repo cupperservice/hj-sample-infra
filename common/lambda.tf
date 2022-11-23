c1ced1d7d9a98107736f00f49cd6bbdfb3f9fd62
kawasima kazuhisa <cupper@mac.com>
2022-11-23 21:05:45 +0900

    modify

--- common/lambda.tf
+++ common/lambda.tf
@@ -11,18 +11,26 @@ data "archive_file" "layer" {
   excludes    = ["../lambda/thumbnail.js"]
 }
 
+resource "null_resource" "npm_install" {
+  provisioner "local-exec" {
+    command = "cd ../lambda/src/nodejs; npm install"
+  }
+}
+
 resource "aws_lambda_function" "thumbnail" {
-  filename  = "${var.lambda.function_file}"
-  function_name = "thumbnail"
-  role          = "${var.lambda.role}"
-  handler       = "thumbnail.handler"
-  layers        = [aws_lambda_layer_version.sharp.arn]
-  runtime       = "nodejs16.x"
-  timeout       = 30
+  filename          = "${var.lambda.function_file}"
+  function_name     = "thumbnail"
+  role              = "${var.lambda.role}"
+  handler           = "thumbnail.handler"
+  layers            = [aws_lambda_layer_version.sharp.arn]
+  runtime           = "nodejs16.x"
+  timeout           = 30
+  source_code_hash  = data.archive_file.thumbnail.output_base64sha256
 }
 
 resource "aws_lambda_layer_version" "sharp" {
-  filename    = "${var.lambda.layer_file}"
-  layer_name  = "sharp"
+  filename            = "${var.lambda.layer_file}"
+  layer_name          = "sharp"
   compatible_runtimes = ["nodejs16.x"]
+  source_code_hash    = data.archive_file.layer.output_base64sha256
 }
