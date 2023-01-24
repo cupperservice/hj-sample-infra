8c64aa10999d5f5115f28443b9a1ff521cc88ebc
kawasima kazuhisa <cupper@mac.com>
2023-01-25 07:02:24 +0900

    add comment

--- staging/main.tf
+++ staging/main.tf
@@ -1,51 +1,51 @@
 terraform {
-  required_version = "= 1.2.9"
+  required_version = "= 1.2.9"  // terraform --version で表示されたバージョンに変える
 }
 
 module "common" {
   source = "../common"
-  region = "XXXXX"
-  az1 = "XXXXX"
-  az2 = "XXXXX"
+  region = "XXXXX"  // リージョン
+  az1 = "XXXXX"     // アベイラビリティゾーン(az1と違う値)
+  az2 = "XXXXX"     // アベイラビリティゾーン(az2と違う値)
   bastion = {
-    image_id = "XXXXX"
-    key_name = "XXXXX"
+    image_id = "XXXXX"  // Amazon Linux 2 のAMI ID
+    key_name = "XXXXX"  // キーペア
   }
   template = {
-    image_id = "XXXXX"
-    key_name = "XXXXX"
+    image_id = "XXXXX"  // Amazon Linux 2 のAMI ID
+    key_name = "XXXXX"  // キーペア
   }
   database = {
     host              = "localhost"
     port              = "3306"
     db_name           = "mydb"
-    username          = "XXXXX"
-    password          = "XXXXX"
+    username          = "XXXXX" // RDS のマスターユーザー名（値は自分で決める）
+    password          = "XXXXX" // RDS のマスターパスワード（値は自分で決める。英数字8文字以上にすること）
     engine            = "aurora-mysql"
     engine_version    = "5.7.mysql_aurora.2.11.0"
     instance_class    = "db.t3.small"
-    num_of_instances  = "0"
+    num_of_instances  = "1"
   }
   session = {
     table_name = "session-table"
     key_name = "sessionId"
   }
   s3_original = {
-    bucket_name = "XXXXX"
+    bucket_name = "XXXXX"   // S3バケット名（値は自分で決める。ブラウザからアップロードした画像の保管先）
   }
   s3_thumbnail = {
-    bucket_name = "XXXXX"
+    bucket_name = "XXXXX"   // S3バケット名（値は自分で決める。アップロードした画像から作成したサムネイル画像の保管先）
   }
   app = {
-    image_id = "XXXXX"
+    image_id = "XXXXX"      // Amazon Linux 2 のAMI ID
     instance_type = "t2.small"
-    key_name = "XXXXX"
+    key_name = "XXXXX"      // キーペア
     max_size = 0
     min_size = 0
   }
   lambda = {
     function_file = "../lambda/thumbnail.zip"
     layer_file    = "../lambda/sharp.zip"
-    role          = "XXXXX"
+    role          = "XXXXX" // LabRole のARN (https://us-east-1.console.aws.amazon.com/iamv2/home?region=us-east-1#/roles)
   }
 }
